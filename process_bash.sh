c4fab9151e917602f45762496446e33051a802f8
xing-angela <angela_xing@brown.edu>
2025-01-11 15:25:42 -0500

    process for diva data

--- process_bash.sh
+++ process_bash.sh
@@ -9,20 +9,21 @@
 #     --use_optim_params
 
 # ROOT_DIR="/users/axing2/data/public/manip"
-# OUT_DIR="./dataset/book_gt1"
-# SESSION="2025-01-08"
-# ANCHOR_CAMERA="brics-odroid-001_cam0"
-# TEXT_PROMPT="hand"
+ROOT_DIR="./dataset/"
+OUT_DIR="./dataset/soda"
+SESSION="soda"
+ANCHOR_CAMERA="brics-odroid-001_cam0"
+TEXT_PROMPT="soda can"
 
-# python3 scripts/gt/1_object_segment.py \
-#     --ith 14 \
-#     --root_dir $ROOT_DIR \
-#     --out_dir $OUT_DIR \
-#     --session $SESSION \
-#     --use_optim_params \
-#     --save_seg_frame \
-#     --anchor_camera $ANCHOR_CAMERA \
-#     --text_prompt $TEXT_PROMPT
+python3 scripts/gt/1_object_segment.py \
+    --ith 14 \
+    --root_dir $ROOT_DIR \
+    --out_dir $OUT_DIR \
+    --session $SESSION \
+    --use_optim_params \
+    --save_seg_frame \
+    --anchor_camera $ANCHOR_CAMERA \
+    --text_prompt $TEXT_PROMPT
 
 # ROOT_DIR="/dataset"
 # OUT_DIR="./dataset/book_gt1"
@@ -37,19 +38,19 @@
 #     --use_optim_params \
 #     --anchor_camera $ANCHOR_CAMERA
 
-ROOT_DIR="/users/axing2/data/public/manip"
-OUT_DIR="./dataset/book_gt1"
-SESSION="2025-01-08"
-ANCHOR_CAMERA="brics-odroid-001_cam0"
-TEXT_PROMPT="book"
-SCAN_PATH="book_simplified.obj"
+# ROOT_DIR="/users/axing2/data/public/manip"
+# OUT_DIR="./dataset/book_gt1"
+# SESSION="2025-01-08"
+# ANCHOR_CAMERA="brics-odroid-001_cam0"
+# TEXT_PROMPT="book"
+# SCAN_PATH="book_simplified.obj"
 
-python3 scripts/gt/3_track_object.py \
-    --ith 14 \
-    --root_dir $ROOT_DIR \
-    --out_dir $OUT_DIR \
-    --session $SESSION \
-    --use_optim_params \
-    --anchor_camera $ANCHOR_CAMERA \
-    --text_prompt $TEXT_PROMPT \
-    --scan_path $SCAN_PATH
+# python3 scripts/gt/3_track_object.py \
+#     --ith 14 \
+#     --root_dir $ROOT_DIR \
+#     --out_dir $OUT_DIR \
+#     --session $SESSION \
+#     --use_optim_params \
+#     --anchor_camera $ANCHOR_CAMERA \
+#     --text_prompt $TEXT_PROMPT \
+#     --scan_path $SCAN_PATH
